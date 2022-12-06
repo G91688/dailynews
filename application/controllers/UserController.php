312acd7734245c9f3622fcb0bcf3e379c4565c2a
G91688 <gultacmansimova0493@gmail.com>
2022-12-06 02:28:49 +0100

    user template

---
+++ application/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+class UserController extends CI_Controller{
+
+public Function index(){
+
+        echo "index methodu";
+}
+
+public function contact(){
+                echo "contact methodu";
+}
+
+
+
+
+}
